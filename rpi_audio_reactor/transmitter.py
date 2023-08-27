583ee1fb334768f80c27d8c7bd9591131346d4e8
toleary <trevor@trevoroleary.com>
2023-08-26 22:05:04 -0400

    receiving and unpacking working, about to refactor a lot

--- rpi_audio_reactor/transmitter.py
+++ rpi_audio_reactor/transmitter.py
@@ -4,7 +4,7 @@ import struct
 import sys
 import time
 import traceback
-
+from random import normalvariate
 import pigpio
 from nrf24 import *
 
@@ -44,8 +44,45 @@ if __name__ == "__main__":
     # Display the content of NRF24L01 device registers.
     radio.show_registers()
     
-    while True:
-        message = "Hello World"
-        radio.send(27)
-        print(f"We sent a message")
-        time.sleep(1)
+    count = 0
+    print(f'Send to {tx_address}')
+    try:
+        while True:
+
+            # Emulate that we read temperature and humidity from a sensor, for example
+            # a DHT22 sensor.  Add a little random variation so we can see that values
+            # sent/received fluctuate a bit.
+            starting_number = 1
+            r = int(normalvariate(20, 2))
+            g = int(255)
+            b = 21
+            print(f'Pixel Color: r={r}, g={g}, b={b}')
+
+            # Pack temperature and humidity into a byte buffer (payload) using a protocol 
+            # signature of 0x01 so that the receiver knows that the bytes we are sending 
+            # are a temperature and a humidity (see "simple-receiver.py").
+            payload = struct.pack("@BBBB", starting_number, r, g, b)
+
+            # Send the payload to the address specified above.
+            radio.reset_packages_lost()
+            radio.send(payload)
+            try:
+                radio.wait_until_sent()
+                
+            except TimeoutError:
+                print("Timeout waiting for transmission to complete.")
+                time.sleep(10)
+                continue
+            
+            if radio.get_packages_lost() == 0:
+                print(f"Success: lost={radio.get_packages_lost()}, retries={radio.get_retries()}")
+            else:
+                print(f"Error: lost={radio.get_packages_lost()}, retries={radio.get_retries()}")
+
+            # Wait 10 seconds before sending the next reading.
+            time.sleep(1)
+
+    except:
+        traceback.print_exc()
+        radio.power_down()
+        pi.stop()
